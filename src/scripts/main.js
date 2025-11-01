b7643ccdd779d5d44494bb274c2cdc379b99a774
Paranjoy <paranjoyhazarika@gmail.com>
2025-11-01 16:19:33 +0530

    Added the sidebar and fixed some issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,21 @@ import '../style/style.css'
 import '../style/responsive.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+
+const sidebarOpenBtn = document.querySelector(".hamburger-menu");
+const sidebarCloseBtn = document.querySelector(".sidebar-cls");
+const sidebar = document.querySelector(".sidebar");
+
+sidebarOpenBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    sidebar.style.transform = "translateX(0%)"
+    document.body.style.overflowY = 'hidden';
+})
+
+sidebarCloseBtn.addEventListener('click', (e) => {
+    e. preventDefault();
+
+    sidebar.style.transform = "translateX(100%)";
+    document.body.style.overflowY = 'scroll';
+})
